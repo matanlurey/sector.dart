3fc0cf72e1a4c40371a779ab80fdce5c267af0e0
Matan Lurey <matan@lurey.org>
2024-07-17 15:09:29 -0700

    Move some test files around.

--- test/prelude.dart
+++ test/prelude.dart
@@ -2,4 +2,4 @@ export 'package:checks/checks.dart';
 export 'package:sector/sector.dart';
 export 'package:test/test.dart' show group, setUp, test;
 
-export 'test_lib.dart';
+export 'src/test_lib.dart';
