d22dd5823f890b87b9d85334d69ba8c3b1c44fc2
Matan Lurey <matan@lurey.org>
2024-07-18 18:27:27 -0700

    ++

--- test/pathfinding/heuristic_search_test.dart
+++ test/pathfinding/heuristic_search_test.dart
@@ -24,7 +24,14 @@ void main() {
   final start = grid.cells.firstWhere((cell) => cell.$2 == 'S').$1;
   final goal = grid.cells.firstWhere((cell) => cell.$2 == 'X').$1;
   final waypoint = grid.cells.firstWhere((cell) => cell.$2 == 'W').$1;
-  final graph = grid.asWeighted(weight: (_, __, ___) => 1);
+  final graph = grid.asWeighted(
+    weight: (a, b, ___) {
+      if (a == '#' || b == '#') {
+        return double.infinity;
+      }
+      return 1;
+    },
+  );
 
   late TestTracer<Pos> tracer;
 
@@ -41,25 +48,37 @@ void main() {
     );
 
     check(path).nodesBetween(start, goal);
-    check(cost).equals(12);
-    check(tracer.events.whereType<VisitEvent<Pos>>()).length.equals(166);
+    check(cost).equals(22);
+    check(tracer.events.whereType<VisitEvent<Pos>>()).length.equals(130);
   });
 
-  test('greedy succeeds with no accurate guidance', () {
-    final (path, cost) = greedyBestFirstSearch.findBestPath<Pos>(
+  test('greedy fails with no accurate guidance', () {
+    final (path, _) = greedyBestFirstSearch.findBestPath<Pos>(
       graph,
       start,
       Goal.node(goal),
-      Heuristic.always(1),
+      Heuristic.zero(),
       tracer: tracer,
     );
 
-    check(path).nodesBetween(start, goal);
-    check(cost).equals(22);
-    check(tracer.events.whereType<VisitEvent<Pos>>()).length.equals(22);
+    check(path).notFound();
+    check(tracer.events.whereType<VisitEvent<Pos>>()).length.equals(35);
   });
 
-  test('greedy should find a path faster with a "waypoint" heuristic', () {
+  test('greedy fails with a manhattan heuristic', () {
+    final (path, _) = greedyBestFirstSearch.findBestPath<Pos>(
+      graph,
+      start,
+      Goal.node(goal),
+      GridHeuristic.manhattan(goal),
+      tracer: tracer,
+    );
+
+    check(path).notFound();
+    check(tracer.events.whereType<VisitEvent<Pos>>()).length.equals(63);
+  });
+
+  test('greedy should find a path with a custom "waypoint" heuristic', () {
     var atWaypoint = false;
     final (path, _) = greedyBestFirstSearch.findBestPath<Pos>(
       graph,
@@ -85,61 +104,33 @@ void main() {
     check(tracer.events.whereType<VisitEvent<Pos>>()).length.equals(32);
   });
 
-  test('a* should find the shortest path with a "waypoint" heuristic', () {
+  test('a* should find the shortest path with a "manhattan" heuristic', () {
     tracer = TestTracer<Pos>(expectRepeatedVisits: true);
 
-    var atWaypoint = false;
     final (path, cost) = astar.findBestPath<Pos>(
       graph,
       start,
       Goal.node(goal),
-      Heuristic((next) {
-        if (atWaypoint) {
-          return 1;
-        }
-
-        // Guide towards 'waypoint' until we hit it.
-        if (next == waypoint) {
-          atWaypoint = true;
-          return 1;
-        }
-
-        // Return guidance towards the waypoint.
-        return manhattan(next, waypoint).toDouble();
-      }),
+      GridHeuristic.manhattan(goal),
       tracer: tracer,
     );
     check(path).nodesBetween(start, goal);
-    check(cost).equals(13);
-    check(tracer.events.whereType<VisitEvent<Pos>>()).length.equals(193);
+    check(cost).equals(22);
+    check(tracer.events.whereType<VisitEvent<Pos>>()).length.equals(88);
   });
 
-  test('fringe should find the shortest path with a "waypoint" heuristic', () {
+  test('fringe should find the shortest path with a "manhattan" heuristic', () {
     tracer = TestTracer<Pos>(expectRepeatedVisits: true);
 
-    var atWaypoint = false;
     final (path, cost) = fringeAstar.findBestPath<Pos>(
       graph,
       start,
       Goal.node(goal),
-      Heuristic((next) {
-        if (atWaypoint) {
-          return 1;
-        }
-
-        // Guide towards 'waypoint' until we hit it.
-        if (next == waypoint) {
-          atWaypoint = true;
-          return 1;
-        }
-
-        // Return guidance towards the waypoint.
-        return manhattan(next, waypoint).toDouble();
-      }),
+      GridHeuristic.manhattan(goal),
       tracer: tracer,
     );
     check(path).nodesBetween(start, goal);
-    check(cost).equals(12);
-    check(tracer.events.whereType<VisitEvent<Pos>>()).length.equals(333);
+    check(cost).equals(22);
+    check(tracer.events.whereType<VisitEvent<Pos>>()).length.equals(182);
   });
 }
